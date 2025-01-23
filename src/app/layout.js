2d68f62d08ebf037c7c9656ee214fec2a20911eb
obidyhasan <obidyhasan@gmail.com>
2025-01-23 16:13:53 +0600

    include suppressHydrationWarning

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,7 +18,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" data-theme="light">
+    <html lang="en" data-theme="light" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
