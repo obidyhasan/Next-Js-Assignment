8b607b6253020c578988b34dd6c5093660d92129
obidyhasan <obidyhasan@gmail.com>
2025-01-23 14:02:06 +0600

    include fetch data from api & show in home section

--- src/app/(home)/page.js
+++ src/app/(home)/page.js
@@ -1,6 +1,10 @@
 import TitleSection from "@/components/TitleSection";
+import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const blogs = await res.json();
+
   return (
     <div className="max-w-7xl mx-auto px-5">
       <div>
@@ -10,6 +14,19 @@ export default function Home() {
             "A collection of articles to expand your knowledge, enhance your skills, and keep you updated on trends."
           }
         ></TitleSection>
+
+        <div className="my-16">
+          {blogs.map((blog) => (
+            <h2
+              key={blog.id}
+              className="font-medium text-lg my-3 hover:text-blue-600 hover:underline"
+            >
+              <Link href={`/blog/${blog.id}`}>
+                {blog.id}. {blog.title}
+              </Link>
+            </h2>
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/components/TitleSection.jsx
+++ src/components/TitleSection.jsx
@@ -1,6 +1,6 @@
 const TitleSection = ({ title, description }) => {
   return (
-    <div className="my-5 text-center">
+    <div className="my-8 text-center">
       <h1 className="font-bold text-3xl">{title}</h1>
       <p className="text-gray-500 mt-2 max-w-xl mx-auto">{description}</p>
     </div>
