be4dd564ee2bfa51b81772dd78b73533949dab73
obidyhasan <obidyhasan@gmail.com>
2025-01-23 14:45:54 +0600

    include blog details section

--- src/app/(home)/blog/[blogId]/page.jsx
+++ src/app/(home)/blog/[blogId]/page.jsx
@@ -1,7 +1,14 @@
-const page = () => {
+const page = async ({ params }) => {
+  const { blogId } = await params;
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${blogId}`
+  );
+  const blog = await res.json();
+
   return (
-    <div>
-      <h1>Blog Details Page</h1>
+    <div className="max-w-7xl mx-auto px-5 my-10">
+      <h1 className="text-2xl font-semibold ">{blog.title}</h1>
+      <p className="text-gray-600 mt-4">{blog.body}</p>
     </div>
   );
 };
