8de79b62d3973b2a97cc6a79e251c4c3ffa987ca
obidyhasan <obidyhasan@gmail.com>
2025-01-23 14:55:17 +0600

    include loading page

---
+++ src/app/(home)/loading.jsx
@@ -0,0 +1,9 @@
+const loading = () => {
+  return (
+    <div className="w-full h-full flex items-center justify-center my-40 bg-white">
+      <span className="loading loading-spinner loading-lg"></span>
+    </div>
+  );
+};
+
+export default loading;
